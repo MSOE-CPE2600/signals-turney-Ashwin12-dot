2721f997243424ee9960c719d13f20c27c600610
Ashwin Senthil <senthilmurugans@msoe.edu>
2025-11-10 16:31:14 -0600

    finished lab

--- recv_signal.c
+++ recv_signal.c
@@ -4,3 +4,45 @@
  * 
  * Brief summary of program:
  */
+
+
+ #include <stdio.h>
+ #include <signal.h>
+ #include <unistd.h>
+
+
+ void sigusr1_handler(int sig, siginfo_t *info, void *uncontext){
+    //im going to do what i did last time for sigaction -> zero out data first and clear warnings
+
+    //this does nothing, legit j uses the parameters so avoid warnings
+    (void) sig; 
+    (void) uncontext; 
+
+    printf("\n Received SIGURS1 from process ID! ID is %d \n", info -> si_pid); //go to struct and receive process ID
+    printf("\n Received value: %d\n", info -> si_value.sival_int); //get the value from the struct
+    
+ }
+
+ int main(void) {
+    //this part is basically sigaction -- im gonna copy my code from sigaction
+
+     struct sigaction sa; 
+
+    //Zero out struct and setup the handler i made
+    sa.sa_sigaction = sigusr1_handler;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask); //clear data -- have to use empty to zero the mask
+
+    //use sigaction for receiving signals
+
+    //do this when this signal arrives -- what sigaction does
+    sigaction(SIGUSR1, &sa, NULL);
+
+    printf("PID from receiver: %d\n", getpid());
+    printf("Waiting for signal . . . \n");
+
+    while (1){
+        pause(); //when u get signal
+    }
+
+ }
--- send_signal.c
+++ send_signal.c
@@ -4,3 +4,32 @@
  * 
  * Brief summary of program:
  */
+
+
+//so we need to use random
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <signal.h>
+ #include <time.h>
+
+ int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("usage: %s\n", argv[0]);
+        return 1;
+    }
+
+    //we need a union - never used it b4, researched it and used it
+    //sigqueue requires it
+    pid_t pid = atoi(argv[1]); //get the PID 
+    union sigval value; //we need this for sigqueue it will not work if we don't
+
+    srand(time(NULL)); //generate a random time 
+    value.sival_int = rand () % 100; //send a random value
+
+    printf("sending sigusr1 to pid %d with value %d\n", pid, value.sival_int); 
+
+    sigqueue(pid, SIGUSR1, value); //send the signal!!
+    return 0;
+ }
+
+
